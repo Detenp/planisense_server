51343d38562c0429b6bb1428adc4d16973ca2112
lounes.bounouar <lounes.bounouar@epita.fr>
2022-03-13 18:23:35 +0100

    corrected throw on main

--- src/main/java/com/lbounouar/app/App.java
+++ src/main/java/com/lbounouar/app/App.java
@@ -2,10 +2,11 @@ package com.lbounouar.app;
 
 import com.lbounouar.app.server.Api;
 
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class App {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, IOException {
         System.out.println("Hello World!");
         Api api = new Api();
         api.start();
